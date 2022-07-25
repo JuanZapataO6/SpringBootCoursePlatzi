020c9d4ca6e75cebd86e85358d3057d11bd243d0
JuanZapataO6 <juan.zapataoc@gmail.com>
2022-07-25 15:30:20 -0500

    First Commit

---
+++ src/test/java/com/PlatziFundaments/SpringBoot/Fundaments/FundamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlatziFundaments.SpringBoot.Fundaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
